c8c115567acbfd7f75fd42f5ba472782e7f55317
Luke Murphey <Luke@LukeMurphey.net>
2014-02-15 13:31:30 -0600

    Fixed loading message that did not disappear when a new lookup file view was loaded

--- src/appserver/modules/LookupFileEditor/LookupFileEditor.js
+++ src/appserver/modules/LookupFileEditor/LookupFileEditor.js
@@ -226,15 +226,16 @@ function doSaveLookup(){
 
 function showDefaultContent(){
 	var data = [
-	            ["Cars per year", "Maserati", "Mazda", "Mercedes", "Mini", "Mitsubishi"],
-	            ["2009", 0, 2941, 4303, 354, 5814],
-	            ["2010", 5, 2905, 2867, 412, 5284],
-	            ["2011", 4, 2517, 4822, 552, 6127],
-	            ["2012", 2, 2422, 5399, 776, 4151]
+	            ["Column1", "Column2", "Column3", "Column4", "Column5", "Column6"],
+	            ["", "", "", "", "", ""],
+	            ["", "", "", "", "", ""],
+	            ["", "", "", "", "", ""],
+	            ["", "", "", "", "", ""]
 	          ];
 	
-	setupTable(null);
+	setupTable(data);
 	$("#tableEditor").show();
+	$(".table-loading-message").hide();
 
 }
 
